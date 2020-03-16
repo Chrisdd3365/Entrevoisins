7d12feb291efc9b34d3a2fdde525ff7fc9b8461d
Christophe DURAND <cdurand.dev@gmail.com>
2020-03-16 20:04:06 +0100

    NeighbourDetailsActivity with layout and onClick into ViewHolder implemented

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighbourDetailsActivity.java
@@ -0,0 +1,12 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NeighbourDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
