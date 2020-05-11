4f8abdfa4192c0a9b9e53cd66480734fc4e97f81
Christophe DURAND <cdurand.dev@gmail.com>
2020-05-11 21:22:14 +0200

    fix unit tests and instrumentalized tests

--- app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
+++ app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
@@ -5,6 +5,8 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.openclassrooms.entrevoisins.R;
+import com.openclassrooms.entrevoisins.di.DI;
+import com.openclassrooms.entrevoisins.model.Neighbour;
 import com.openclassrooms.entrevoisins.ui.neighbour_list.ListNeighbourActivity;
 import com.openclassrooms.entrevoisins.utils.DeleteViewAction;
 
@@ -12,6 +14,9 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -22,10 +27,11 @@ import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.openclassrooms.entrevoisins.utils.RecyclerViewItemCountAssertion.withItemCount;
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.core.IsNull.notNullValue;
-
+import static org.hamcrest.core.StringContains.containsString;
 
 
 /**
@@ -79,41 +85,38 @@ public class NeighboursListTest {
     }
 
     /**
-     * When we click on 1 item, it shows the details activity
+     * When we click on 1 item, it shows the details activity of this item
      */
     @Test
     public void myNeighboursList_onClickListener_shouldShowDetailsActivity() {
         // When perform a click on a item
         onView(allOf(isDisplayed(), withId(R.id.list_neighbours) ) ).perform(actionOnItemAtPosition(0, click() ) );
-    }
-
-    /**
-     * When we click on 1 item, check if it shows the name of the neighbour in the details activity
-     */
-    @Test
-    public void myNeighboursList_onClickListener_shouldShowNeighbourName() {
-        // When perform a click on a item, it shows the details activity
-        onView(allOf(isDisplayed(), withId(R.id.list_neighbours) ) ).perform(actionOnItemAtPosition(0, click() ) );
-        // Then: check if the name of the neighbour is displayed in the details activity
+        // Check if the name's text view of the neighbour is displayed in the details activity
         onView(withId(R.id.user_name) ).check(matches(isDisplayed() ) );
+        // Then: check if the name of the neighbour is the expected one
+        onView(withId(R.id.user_name)).check(matches(withText(containsString(DI.getNeighbourApiService().getNeighbours().get(0).getName() ) ) ) );
     }
 
     /**
      * When we add a neighbour into the favorites neighbours list, check if it displays only the neighbour set as favorite neighbour
      */
     @Test
-    public void myFavoritesNeighboursList_onClickListener_shouldShowFavoriteNeighbour() {
+    public void myFavoritesNeighboursList_onClickListener_shouldShowFavoriteNeighbourIntoFavoriteList() {
         // When perform a click on a item, it shows the details activity
         onView(allOf(isDisplayed(), withId(R.id.list_neighbours) ) ).perform(actionOnItemAtPosition(4, click() ) );
-        // Then: perform a click on favorite button
+        // Perform a click on favorite button
         onView(withId(R.id.favorite_image_button) ).perform(click() );
         // Going back to the list neighbours activity
         onView(withId(R.id.back_button) ).perform(click() );
         // Display favorites neighbours list by clicking on the second tab item of view pager
         onView(withId(R.id.container) ).perform(swipeLeft() );
-        // Given: only this item is displayed into the favorites neighbours list
+        // Only this item is displayed into the favorites neighbours list
         onView(allOf(isDisplayed(), withId(R.id.item_list_avatar) ) ).check(matches(isDisplayed() ) );
         onView(allOf(isDisplayed(), withId(R.id.item_list_name) ) ).check(matches(isDisplayed() ) );
+        // Then: check if the name and the avatar are the expected ones
+        onView(allOf(isDisplayed(), withId(R.id.item_list_avatar) ) ).check(matches(withText(containsString(DI.getNeighbourApiService().getNeighbours().get(4).getAvatarUrl() ) ) ) );
+        onView(allOf(isDisplayed(), withId(R.id.item_list_name) ) ).check(matches(withText(containsString(DI.getNeighbourApiService().getNeighbours().get(4).getName() ) ) ) );
+
     }
 
 }
